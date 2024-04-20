7a2c4cc5f95865c1a93dd381f1890a85a756329d
Anton <antekiv@github.com>
2024-04-20 08:33:03 +0300

    Added version

---
+++ lib.cpp
@@ -0,0 +1,10 @@
+//
+// Created by anton on 19.4.24.
+//
+#include "lib.h"
+
+#include "version.h"
+
+int version() {
+    return PROJECT_VERSION_PATCH;
+}
---
+++ lib.h
@@ -0,0 +1,6 @@
+//
+// Created by anton on 19.4.24.
+//
+#pragma once
+
+int version();
